e39dc2dc392ace724a4e87575bbd8142be6f2809
RISR3 <risierra.3@gmail.com>
2020-04-20 08:54:38 -0400

    Moving folders

---
+++ TicTacToeServerPlugin - Copy/TicTacToeServerPlugin/Scripts/Networking/tags.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Scripts.Networking
+{
+    class Tags
+    {
+
+        public enum Tag
+        {
+            SET_NAME,
+            GOT_MATCH,
+            SLATE_TAKEN,
+            SERVER_CONFIRM_SLATE_TAKEN,
+            GOT_MTACH
+        }
+
+    }
+}
